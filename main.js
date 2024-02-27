98d72babb31770590ab481d78fecfefa709d4160
Vitão Rei Delas <127319166+Victorreidelas@users.noreply.github.com>
2024-02-27 10:11:44 -0300

    db

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ function mudarCorFundo(){
     document.querySelector("#p1").style.color= "green"
 }
     
+function alterarTexto(){
+    textoForm = document.querySelector("#textinput"). value
+
+    document.querySelector("#texto2").innerHTML = textoForm
+}
